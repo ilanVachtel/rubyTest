e3d943ed5224f4904549e2186fd7cf1b9f2124a0
ilanVachtel <ilan.vachtel@worthy.com>
2019-06-11 23:30:41 +0300

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'item/index'
+  get 'items_controller/index'
+
+  resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
